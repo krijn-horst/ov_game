5a14596b99362775b7f90d80a0cc3517ef72cad2
tstoof <tamara.stoof@gmail.com>
2026-08-02 10:51:45 +0200

    added protectedroute

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,28 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+interface ProtectedRouteProps {
+    children: React.ReactNode;
+}
+
+const ProtectedRoute = ({
+    children
+}: ProtectedRouteProps) => {
+
+    const {
+        user,
+        isGuest
+    } = useAuth();
+
+    if (!user && !isGuest) {
+        return (
+            <Navigate
+                to="/login"
+                replace
+            />
+        );
+    }
+
+    return children;
+};
+export default ProtectedRoute;
